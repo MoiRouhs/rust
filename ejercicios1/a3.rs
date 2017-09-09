24af522e1c7008560775cad8c3d188fb5f340416
Moi Rouhs <carsrouhs@gmail.com>
2017-09-09 00:36:57 +0000

    correcciones generales y subida de tercer ejercicio

---
+++ ejercicios1/a3.rs
@@ -0,0 +1,16 @@
+fn main() {  // Sentencia IF y cenversion de &str <=> String. 
+
+  let texto1: &str = "hola";
+  let texto2 = "hola".to_string();
+  
+  let text1 = &texto1.to_string(); // Conversion de &str a String es inportanteno olvidar el "&"
+  let text2: &str = &texto2; // Conversion de String a &str es importante declarar el tipo de enlace a variable y ponerle el "&"
+  
+  if texto1 == texto2{ // El IF de Rust tiene un comportamiento muy similar al de los lenguajes de tipado dinamico o tipado debil.
+    println!("Son iguales {}",text1);
+  }else if text1.len() == text2.len() { // .len() sirve para saber el largo de una cadena en este caso.
+    println!("Este es el else if de rust");
+  }else{
+    println!("Son diferente {}",text2);
+  };
+}
