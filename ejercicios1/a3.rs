ebf789f956422ca745fa81906432c506f5307c1c
Moi Rouhs <carsrouhs@gmail.com>
2017-09-21 20:45:00 +0000

    Correcciones ortográficas ya que tengo tildes

--- ejercicios1/a3.rs
+++ ejercicios1/a3.rs
@@ -1,12 +1,12 @@
-fn main() {  // Sentencia IF y cenversion de &str <=> String. 
+fn main() {  // Sentencia IF y cenversión de &str <=> String. 
 
   let texto1: &str = "hola";
   let texto2 = "hola".to_string();
   
-  let text1 = &texto1.to_string(); // Conversion de &str a String es inportanteno olvidar el "&"
-  let text2: &str = &texto2; // Conversion de String a &str es importante declarar el tipo de enlace a variable y ponerle el "&"
+  let text1 = &texto1.to_string(); // Conversión de &str a String es inportanteno olvidar el "&"
+  let text2: &str = &texto2; // Conversión de String a &str es importante declarar el tipo de enlace a variable y ponerle el "&"
   
-  if texto1 == texto2{ // El IF de Rust tiene un comportamiento muy similar al de los lenguajes de tipado dinamico o tipado debil.
+  if texto1 == texto2{ // El IF de Rust tiene un comportamiento muy similar al de los lenguajes de tipado dinámico o tipado debil.
     println!("Son iguales {}",text1);
   }else if text1.len() == text2.len() { // .len() sirve para saber el largo de una cadena en este caso.
     println!("Este es el else if de rust");
