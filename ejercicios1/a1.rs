e067b165350cfde95880640ae98c508b64b2ccc9
Moi Rouhs <carsrouhs@gmail.com>
2017-09-22 00:30:38 +0000

    Última corrección

--- ejercicios1/a1.rs
+++ ejercicios1/a1.rs
@@ -1,4 +1,4 @@
-fn main() { // Todo programa escrito en rust debe teneruna función principal denominada main.
+fn main() { // Todo programa escrito en rust debe tener una función principal denominada main.
   println!("hola mundo"); // forma básica de imprimir hola mundo en la terminal.
 // Para ejecutar el programa es necesario compilarlos con "rustc a1.rs"(a1.rs = nombre del archivo) y despues "./ai"
 
